df63f34ea7c21588b8b97deb98b21d88a3da7a49
yujinjiang <2879032410@qq.com>
2019-02-01 21:43:16 +0800

    add goodList

---
+++ vue/goodList/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="goodList">
+        <div class="goodsItems">
+            <div class="goodsImg">
+                <a href="#">
+                    <img src="../../images/aboutUs/bg.jpg" alt="">
+                </a>
+            </div>
+            <div class="goodsPrice">
+                <em><b>￥</b></em>
+                <span class="average">6元/个</span>
+            </div>
+            <div class="goodsTitle">
+                <a href="#">
+                    Zespri 佳沛新西兰阳光金
+                </a>
+            </div>
+            <div class="goodsStatus">
+                <span>月成交 <em>6000</em>笔</span>
+                <span class="norightborder">月评价 <em>6000</em>个</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+
+<style lang="scss">
+    #goodList {
+        width: 1210px;
+        height: auto;
+        margin: 20px auto 0;
+    }
+
+    #goodList .goodsItems {
+        width: 220px;
+        height: 330px;
+        margin: 0 22px 22px 0;
+    }
+
+    #goodList .goodsItems .goodsItemsWarp {
+        width: 210px;
+        height: 315px;
+        background-color: #fff;
+        margin: 0;
+        padding: 4px;
+        border: 1px solid #f5f5f5;
+        border-radius: 3px;
+    }
+</style>
