b163e0fc380203bef0b6be27fc245ebcb99de8eb
yujinjiang <2879032410@qq.com>
2019-02-17 23:32:22 +0800

    s

---
+++ vue/vuex/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        {{username}}
+    </div>
+</template>
+
+<script>
+    import store from '../../stores/vuex';
+    import {
+        mapState
+    } from 'vuex';
+
+    export default {
+        store,
+        provide: {
+            age: 24
+        },
+        computed: {
+            ...mapState(['username'])
+        },
+        mounted() {
+            console.log(this.$store, this._provided);
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
