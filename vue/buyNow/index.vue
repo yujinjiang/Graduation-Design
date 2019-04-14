f42aee7d6d4edf7cff977ac34a7759ccfb9dcc11
yujinjiang <2879032410@qq.com>
2019-04-14 11:03:41 +0800

    增加直接购买

---
+++ vue/buyNow/index.vue
@@ -0,0 +1,88 @@
+<template>
+    <section class="view">
+
+        <Card>
+            <p slot="title">
+                <Icon type="ios-basket" />
+                订单详情
+            </p>
+            <a href="#" slot="extra">
+                <Icon type="md-bookmark" />
+            </a>
+            <ul class="view-content-item-list">
+                <li class="view-content-item-list-inner">
+                    <img src="https://img.alicdn.com/imgextra/i4/98055677/O1CN01K4fKfq1ro6yKwoMm1_!!0-saturn_solar.jpg_220x220.jpg_.webp" alt="">
+                </li>
+                <ul class="view-content-item-list-inner">
+
+                    <li>
+                        价格：10
+                    </li>
+                    <li>
+                        姓名：于锦江
+                    </li>
+                    <li>
+                        收货地址：北京市海淀区 北分厂家属区 16号楼三单元401
+                    </li>
+                    <li>
+                        联系电话：17611499307
+                    </li>
+                    <Button type="primary">确认</Button>
+                    <Button type="primary">取消</Button>
+                </ul>
+            </ul>
+        </Card>
+    </section>
+
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                value1: '1'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    body {
+        height: 100%;
+        background-color: #de335e !important;
+        overflow: scroll;
+    }
+
+    $content-width: 1200px;
+    .view {
+        background-color: #fff;
+        margin: 30px auto;
+        padding: 30px;
+        width: $content-width;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        box-shadow: 0 0 10px #333;
+
+        &-content {
+            margin-top: 30px;
+            overflow: scroll;
+
+            &-item {
+                margin-bottom: 30px;
+            }
+
+            &-item-list {
+                font-size: 0;
+
+                &-inner {
+                    display: inline-block;
+                    vertical-align: top;
+
+                    &:last-child {
+                        margin-left: 30px;
+                        font-size: 14px;
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ vue/viewGoods/index.vue
@@ -0,0 +1,276 @@
+<template>
+    <section class="view">
+        <Input search enter-button="Search" placeholder="Enter name..." />
+        <main class="view-content">
+            <div class="view-content-item">
+                <Card>
+                    <p slot="title">
+                        <Icon type="ios-cart" />
+                        现货3册正版书原版原著呼兰河传城南旧事童年商务印书馆萧红著林海音高尔基语文新课标小学生版四五下册六5年级课外必读书籍无删减
+                    </p>
+                    <a href="#" slot="extra">
+                        <Icon type="ios-loop-strong"></Icon>
+                        查看详情
+                    </a>
+                    <ul class="view-content-item-list">
+                        <li class="view-content-item-list-inner">
+                            <img src="https://img.alicdn.com/imgextra/i4/98055677/O1CN01K4fKfq1ro6yKwoMm1_!!0-saturn_solar.jpg_220x220.jpg_.webp" alt="">
+                        </li>
+                        <li class="view-content-item-list-inner">
+                            <div>价格：30</div>
+                            <div>想要：10人</div>
+                            <div>收藏：20人</div>
+                            <Button type="primary">
+                                购买
+                            </Button>
+                            <Button type="primary">
+                                收藏
+                            </Button>
+                            <Button type="primary">
+                                想要
+                            </Button>
+                        </li>
+                    </ul>
+                </Card>
+            </div>
+            <div class="view-content-item">
+                <Card>
+                    <p slot="title">
+                        <Icon type="ios-cart" />
+                        现货3册正版书原版原著呼兰河传城南旧事童年商务印书馆萧红著林海音高尔基语文新课标小学生版四五下册六5年级课外必读书籍无删减
+                    </p>
+                    <a href="#" slot="extra">
+                        <Icon type="ios-loop-strong"></Icon>
+                        查看详情
+                    </a>
+                    <ul class="view-content-item-list">
+                        <li class="view-content-item-list-inner">
+                            <img src="https://img.alicdn.com/imgextra/i2/28851861/O1CN01NoM9eG1PcNaVs60id_!!0-saturn_solar.jpg_220x220.jpg_.webp" alt="">
+                        </li>
+                        <li class="view-content-item-list-inner">
+                            <div>价格：30</div>
+                            <div>想要：10人</div>
+                            <div>收藏：20人</div>
+                            <Button type="primary">
+                                购买
+                            </Button>
+                            <Button type="primary">
+                                收藏
+                            </Button>
+                            <Button type="primary">
+                                想要
+                            </Button>
+                        </li>
+                    </ul>
+                </Card>
+            </div>
+            <div class="view-content-item">
+                <Card>
+                    <p slot="title">
+                        <Icon type="ios-cart" />
+                        现货3册正版书原版原著呼兰河传城南旧事童年商务印书馆萧红著林海音高尔基语文新课标小学生版四五下册六5年级课外必读书籍无删减
+                    </p>
+                    <a href="#" slot="extra">
+                        <Icon type="ios-loop-strong"></Icon>
+                        查看详情
+                    </a>
+                    <ul class="view-content-item-list">
+                        <li class="view-content-item-list-inner">
+                            <img src="https://img.alicdn.com/imgextra/i2/27766371/O1CN011wvxnVU09q6dQEJ_!!0-saturn_solar.jpg_220x220.jpg_.webp" alt="">
+                        </li>
+                        <li class="view-content-item-list-inner">
+                            <div>价格：30</div>
+                            <div>想要：10人</div>
+                            <div>收藏：20人</div>
+                            <Button type="primary">
+                                购买
+                            </Button>
+                            <Button type="primary">
+                                收藏
+                            </Button>
+                            <Button type="primary">
+                                想要
+                            </Button>
+                        </li>
+                    </ul>
+                </Card>
+            </div>
+            <div class="view-content-item">
+                <Card>
+                    <p slot="title">
+                        <Icon type="ios-cart" />
+                        现货3册正版书原版原著呼兰河传城南旧事童年商务印书馆萧红著林海音高尔基语文新课标小学生版四五下册六5年级课外必读书籍无删减
+                    </p>
+                    <a href="#" slot="extra">
+                        <Icon type="ios-loop-strong"></Icon>
+                        查看详情
+                    </a>
+                    <ul class="view-content-item-list">
+                        <li class="view-content-item-list-inner">
+                            <img src="https://img.alicdn.com/imgextra/i4/98055677/O1CN01K4fKfq1ro6yKwoMm1_!!0-saturn_solar.jpg_220x220.jpg_.webp" alt="">
+                        </li>
+                        <li class="view-content-item-list-inner">
+                            <div>价格：30</div>
+                            <div>想要：10人</div>
+                            <div>收藏：20人</div>
+                            <Button type="primary">
+                                购买
+                            </Button>
+                            <Button type="primary">
+                                收藏
+                            </Button>
+                            <Button type="primary">
+                                想要
+                            </Button>
+                        </li>
+                    </ul>
+                </Card>
+            </div>
+            <div class="view-content-item">
+                <Card>
+                    <p slot="title">
+                        <Icon type="ios-cart" />
+                        现货3册正版书原版原著呼兰河传城南旧事童年商务印书馆萧红著林海音高尔基语文新课标小学生版四五下册六5年级课外必读书籍无删减
+                    </p>
+                    <a href="#" slot="extra">
+                        <Icon type="ios-loop-strong"></Icon>
+                        查看详情
+                    </a>
+                    <ul class="view-content-item-list">
+                        <li class="view-content-item-list-inner">
+                            <img src="https://img.alicdn.com/imgextra/i4/98055677/O1CN01K4fKfq1ro6yKwoMm1_!!0-saturn_solar.jpg_220x220.jpg_.webp" alt="">
+                        </li>
+                        <li class="view-content-item-list-inner">
+                            <div>价格：30</div>
+                            <div>想要：10人</div>
+                            <div>收藏：20人</div>
+                            <Button type="primary">
+                                购买
+                            </Button>
+                            <Button type="primary">
+                                收藏
+                            </Button>
+                            <Button type="primary">
+                                想要
+                            </Button>
+                        </li>
+                    </ul>
+                </Card>
+            </div>
+            <div class="view-content-item">
+                <Card>
+                    <p slot="title">
+                        <Icon type="ios-cart" />
+                        现货3册正版书原版原著呼兰河传城南旧事童年商务印书馆萧红著林海音高尔基语文新课标小学生版四五下册六5年级课外必读书籍无删减
+                    </p>
+                    <a href="#" slot="extra">
+                        <Icon type="ios-loop-strong"></Icon>
+                        查看详情
+                    </a>
+                    <ul class="view-content-item-list">
+                        <li class="view-content-item-list-inner">
+                            <img src="https://img.alicdn.com/imgextra/i4/98055677/O1CN01K4fKfq1ro6yKwoMm1_!!0-saturn_solar.jpg_220x220.jpg_.webp" alt="">
+                        </li>
+                        <li class="view-content-item-list-inner">
+                            <div>价格：30</div>
+                            <div>想要：10人</div>
+                            <div>收藏：20人</div>
+                            <Button type="primary">
+                                购买
+                            </Button>
+                            <Button type="primary">
+                                收藏
+                            </Button>
+                            <Button type="primary">
+                                想要
+                            </Button>
+                        </li>
+                    </ul>
+                </Card>
+            </div>
+            <div class="view-content-item">
+                <Card>
+                    <p slot="title">
+                        <Icon type="ios-cart" />
+                        现货3册正版书原版原著呼兰河传城南旧事童年商务印书馆萧红著林海音高尔基语文新课标小学生版四五下册六5年级课外必读书籍无删减
+                    </p>
+                    <a href="#" slot="extra">
+                        <Icon type="ios-loop-strong"></Icon>
+                        查看详情
+                    </a>
+                    <ul class="view-content-item-list">
+                        <li class="view-content-item-list-inner">
+                            <img src="https://img.alicdn.com/imgextra/i4/98055677/O1CN01K4fKfq1ro6yKwoMm1_!!0-saturn_solar.jpg_220x220.jpg_.webp" alt="">
+                        </li>
+                        <li class="view-content-item-list-inner">
+                            <div>价格：30</div>
+                            <div>想要：10人</div>
+                            <div>收藏：20人</div>
+                            <Button type="primary">
+                                购买
+                            </Button>
+                            <Button type="primary">
+                                收藏
+                            </Button>
+                            <Button type="primary">
+                                想要
+                            </Button>
+                        </li>
+                    </ul>
+                </Card>
+            </div>
+        </main>
+    </section>
+
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                value1: '1'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    body {
+        height: 100%;
+        background-color: #de335e !important;
+        overflow: scroll;
+    }
+
+    $content-width: 1200px;
+    .view {
+        background-color: #fff;
+        margin: 30px auto;
+        padding: 30px;
+        width: $content-width;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        box-shadow: 0 0 10px #333;
+
+        &-content {
+            margin-top: 30px;
+            overflow: scroll;
+
+            &-item {
+                margin-bottom: 30px;
+            }
+
+            &-item-list {
+                font-size: 0;
+
+                &-inner {
+                    display: inline-block;
+                    vertical-align: top;
+
+                    &:last-child {
+                        margin-left: 30px;
+                        font-size: 14px;
+                    }
+                }
+            }
+        }
+    }
+</style>
