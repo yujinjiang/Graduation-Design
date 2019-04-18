6f6de6e6b84288cbfb983634f67ded1556990f59
yujinjiang <2879032410@qq.com>
2019-04-18 09:37:45 +0800

    save

---
+++ vue/classification/index.vue
@@ -0,0 +1,232 @@
+<template>
+    <section class="sign-in">
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+            <FormItem label="一级分类" prop="name">
+                <Select v-model="formValidate.name" style="width:200px">
+                    <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                </Select>
+            </FormItem>
+            <FormItem label="二级分类" prop="mail">
+                <Select v-model="formValidate.mail" style="width:200px">
+                    <Option v-for="item in cityList2" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                </Select>
+            </FormItem>
+            <FormItem label="书籍介绍" prop="intro">
+                <Input v-model="formValidate.phone" placeholder="Enter your phone number"></Input>
+            </FormItem>
+            <FormItem label="书籍图片">
+                <div class="demo-upload-list" v-for="item in uploadList">
+                    <template v-if="item.status === 'finished'">
+                        <img :src="item.url">
+                        <div class="demo-upload-list-cover">
+                            <Icon type="ios-eye-outline" @click.native="handleView(item.name)"></Icon>
+                            <Icon type="ios-trash-outline" @click.native="handleRemove(item)"></Icon>
+                        </div>
+                    </template>
+                    <template v-else>
+                        <Progress v-if="item.showProgress" :percent="item.percentage" hide-info></Progress>
+                    </template>
+                </div>
+                <Upload
+                        ref="upload"
+                        :show-upload-list="false"
+                        :default-file-list="defaultList"
+                        :on-success="handleSuccess"
+                        :format="['jpg','jpeg','png']"
+                        :max-size="2048"
+                        :on-format-error="handleFormatError"
+                        :on-exceeded-size="handleMaxSize"
+                        :before-upload="handleBeforeUpload"
+                        multiple
+                        type="drag"
+                        action="//jsonplaceholder.typicode.com/posts/"
+                        style="display: inline-block;width:58px;">
+                    <div style="width: 58px;height:58px;line-height: 58px;">
+                        <Icon type="ios-camera" size="20"></Icon>
+                    </div>
+                </Upload>
+                <Modal title="View Image" v-model="visible">
+                    <img :src="'https://o5wwk8baw.qnssl.com/' + imgName + '/large'" v-if="visible" style="width: 100%">
+                </Modal>
+            </FormItem>
+            <FormItem>
+                <Button type="primary" @click="handleSubmit('formValidate')">Submit</Button>
+            </FormItem>
+        </Form>
+    </section>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                cityList: [
+                    {
+                        label: '工具类',
+                        value: '0'
+                    },
+                    {
+                        label: '旅游类',
+                        value: '1'
+                    },
+                    {
+                        value: '2',
+                        label: '风景类'
+                    },
+                    {
+                        value: '3',
+                        label: '时尚类'
+                    },
+                    {
+                        value: '4',
+                        label: '财经类'
+                    }
+                ],
+                cityList2: [
+                    {
+                        label: '编程语言类',
+                        value: '0'
+                    },
+                    {
+                        label: '通信原理类',
+                        value: '1'
+                    },
+                    {
+                        value: '2',
+                        label: '加密技术类'
+                    }
+                ],
+                formValidate: {
+                    name: '',
+                    mail: '',
+                    phone: '',
+                    radio: ''
+                },
+                ruleValidate: {
+                    name: [
+                        {required: true, message: 'The account cannot be empty', trigger: 'blur'}
+                    ],
+                    phone: [
+                        {
+                            required: true, message: 'The phone cannot be empty',trigger: 'blur'
+                        }
+                    ],
+                    intro: [
+                        {required: true, message: 'Mailbox cannot be empty', trigger: 'blur'},
+                    ]
+
+                },
+
+                defaultList: [],
+                imgName: '',
+                visible: false,
+                uploadList: []
+            }
+        },
+        mounted () {
+            this.uploadList = this.$refs.upload.fileList;
+        },
+        methods: {
+            handleSubmit(name) {
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                        this.$Message.success('Success!');
+                    } else {
+                        this.$Message.error('Fail!');
+                    }
+                })
+            },
+
+            handleView (name) {
+                this.imgName = name;
+                this.visible = true;
+            },
+            handleRemove (file) {
+                const fileList = this.$refs.upload.fileList;
+                this.$refs.upload.fileList.splice(fileList.indexOf(file), 1);
+            },
+            handleSuccess (res, file) {
+                file.url = 'https://o5wwk8baw.qnssl.com/7eb99afb9d5f317c912f08b5212fd69a/avatar';
+                file.name = '7eb99afb9d5f317c912f08b5212fd69a';
+            },
+            handleFormatError (file) {
+                this.$Notice.warning({
+                    title: 'The file format is incorrect',
+                    desc: 'File format of ' + file.name + ' is incorrect, please select jpg or png.'
+                });
+            },
+            handleMaxSize (file) {
+                this.$Notice.warning({
+                    title: 'Exceeding file size limit',
+                    desc: 'File  ' + file.name + ' is too large, no more than 2M.'
+                });
+            },
+            handleBeforeUpload () {
+                const check = this.uploadList.length < 5;
+                if (!check) {
+                    this.$Notice.warning({
+                        title: 'Up to five pictures can be uploaded.'
+                    });
+                }
+                return check;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    body {
+        background-color: #de335e !important;
+    }
+
+    $content-width: 1200px;
+    .sign-in {
+        background-color: #fff;
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        box-sizing: border-box;
+        padding: 30px;
+        width: $content-width;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        box-shadow: 0 0 10px #333;
+    }
+
+    .demo-upload-list{
+        display: inline-block;
+        width: 60px;
+        height: 60px;
+        text-align: center;
+        line-height: 60px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        overflow: hidden;
+        background: #fff;
+        position: relative;
+        box-shadow: 0 1px 1px rgba(0,0,0,.2);
+        margin-right: 4px;
+    }
+    .demo-upload-list img{
+        width: 100%;
+        height: 100%;
+    }
+    .demo-upload-list-cover{
+        display: none;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: rgba(0,0,0,.6);
+    }
+    .demo-upload-list:hover .demo-upload-list-cover{
+        display: block;
+    }
+    .demo-upload-list-cover i{
+        color: #fff;
+        font-size: 20px;
+        cursor: pointer;
+        margin: 0 2px;
+    }
+</style>
